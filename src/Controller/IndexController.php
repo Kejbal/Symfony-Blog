04f9006609b6c79731a7d83c754f44d58b5dd492
Kejbal <kejbal@gmail.com>
2019-02-23 10:37:07 +0100

    add older and newer button on home page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,20 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\BlogPostRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends ControllerBase
 {
     /**
      * @Route("/index", name="index")
      */
-    public function index(BlogPostRepository $blog_post)
+    public function index(Request $request, BlogPostRepository $blog_post)
     {
-        $posts = $blog_post->findBy(['draft' => '0'], ['id' => 'DESC'], 5);
+        $page = (int) $request->attributes->get('page');
+        $limit = 5;
+        $show_button_older = false;
+        $show_button_newer = false;
+
+        if ($page > 1) {
+            $limit_offset = ($page - 1) * $limit;
+            $show_button_newer = true;
+        } else {
+            $limit_offset = 0;
+            $page = 1;
+        }
+
+        $posts = $blog_post->findBy(['draft' => '0'], ['id' => 'DESC'], $limit, $limit_offset);
+
+        $all_posts = $blog_post->findBy(['draft' => '0']);
+        if (count($all_posts) > $limit * $page) {
+            $show_button_older = true;
+        }
 
         $this->_data_view['controller_name'] = 'IndexController';
         $this->_data_view['posts'] = $posts;
+        $this->_data_view['show_button_older'] = $show_button_older;
+        $this->_data_view['show_button_newer'] = $show_button_newer;
+        $this->_data_view['current_page'] = $page;
         return $this->render('index/index.html.twig', $this->_data_view);
     }
 }
--- tests/Controller/IndexTest.php
+++ tests/Controller/IndexTest.php
@@ -6,12 +6,14 @@ use App\Entity\BlogPost;
 
 class IndexTest extends BaseWeb
 {
+
     public function testHome()
     {
+        $per_page = 5;
 
         $crawler = $this->client->request('GET', '/');
 
-        $posts = self::$container->get('doctrine')->getRepository(BlogPost::class)->findBy(['draft' => '0'], ['id' => 'DESC'], 5);
+        $posts = self::$container->get('doctrine')->getRepository(BlogPost::class)->findBy(['draft' => '0'], ['id' => 'DESC'], $per_page);
 
         foreach ($posts as $post) {
 
@@ -19,5 +21,47 @@ class IndexTest extends BaseWeb
             $this->assertEquals(1, $crawler->filter('h3.post-subtitle:contains("' . $post->getSubtitle() . '")')->count());
             $this->assertEquals(1, $crawler->filter('p.post-meta:contains("' . $post->getDate()->format('H:i:s d M Y') . '")')->count());
         }
+
+        $all_posts = self::$container->get('doctrine')->getRepository(BlogPost::class)->findBy(['draft' => '0']);
+        if (count($all_posts) > $per_page) {
+            $this->assertEquals(1, $crawler->filter('#older-posts:contains("Older Posts")')->count());
+            $this->assertEquals(
+                '/2',
+                $crawler->filter('#older-posts a:contains("Older Posts")')->attr('href')
+            );
+
+            $number_page = ceil(count($all_posts) / $per_page);
+
+            for ($page = 2; $page <= $number_page; $page++) {
+                $crawler = $this->client->request('GET', '/' . $page
+                );
+
+                $posts = self::$container->get('doctrine')->getRepository(BlogPost::class)->findBy(['draft' => '0'], ['id' => 'DESC'], $per_page, ($page - 1) * $per_page);
+
+                foreach ($posts as $post) {
+                    $this->assertEquals(1, $crawler->filter('h2.post-title:contains("' . $post->getTitle() . '")')->count());
+                    $this->assertEquals(1, $crawler->filter('h3.post-subtitle:contains("' . $post->getSubtitle() . '")')->count());
+                    $this->assertEquals(1, $crawler->filter('p.post-meta:contains("' . $post->getDate()->format('H:i:s d M Y') . '")')->count());
+                }
+
+                $this->assertEquals(1, $crawler->filter('#newer-posts:contains("Newer Posts")')->count());
+                $newer_page = $page - 1;
+                $this->assertEquals(
+                    '/' . $newer_page,
+                    $crawler->filter('#newer-posts a:contains("Newer Posts")')->attr('href')
+                );
+
+                if ($page < $number_page) {
+                    $this->assertEquals(1, $crawler->filter('#older-posts:contains("Older Posts")')->count());
+                    $older_page = $page + 1;
+                    $this->assertEquals(
+                        '/' . $older_page,
+                        $crawler->filter('#older-posts a:contains("Older Posts")')->attr('href')
+                    );
+                }
+            }
+
+        }
+
     }
 }
