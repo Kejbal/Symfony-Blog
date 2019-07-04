c1595881bc7a430444ce258272a29337ff84a52f
Kejbal <kejbal@gmail.com>
2019-07-04 19:09:04 +0200

    added javascript dependency manager

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
